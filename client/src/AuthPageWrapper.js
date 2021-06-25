51038e4d6aff7c2abb980d6da52101c063dd8b99
bhrg3se <bhargab@seknox.com>
2021-06-25 19:12:23 +0545

    feat: new login/signup design implemented

---
+++ client/src/AuthPageWrapper.js
@@ -0,0 +1,76 @@
+import React from "react";
+
+import {
+    Grid,
+    Typography,
+} from "@material-ui/core";
+import {makeStyles} from "@material-ui/core/styles";
+import BGImage from "./assets/bg-img.png";
+import SVGBUbble from "./assets/bubble.svg";
+
+const useStyles = makeStyles(() => ({
+    leftPortion: {
+        backgroundImage: `url(${BGImage})`,
+        backgroundSize: "cover",
+        height: "100vh",
+        width: "100vh",
+
+    },
+    blueLayer: {
+        backgroundColor: "rgba(59,145,205,0.7)",
+        width: "100%",
+        height: "100%"
+    },
+    bubble: {
+        color: 'blue'
+    },
+    text: {
+        color: 'white'
+    }
+}));
+
+
+const AuthPageWrapper = (props) => {
+    const classes = useStyles()
+
+    return (
+        <Grid container spacing={0} direction={"row"}>
+            <Grid key={1} xs={4} item className={classes.leftPortion}>
+                <Grid className={classes.blueLayer} container direction={"row"} alignItems={"center"}
+                      alignContent={"center"}>
+                    <Grid item xs={12} align={"center"}>
+                        <img alt="Hello" className={classes.bubble} src={SVGBUbble}/>
+                    </Grid>
+                    <Grid item xs={12} align={"center"}>
+                        <Typography
+                            className={classes.text}
+                            variant={"h4"}
+                        >
+                            Converse with anyone <br/>
+                            with any language
+                        </Typography>
+                    </Grid>
+                </Grid>
+
+            </Grid>
+            <Grid
+                key={2}
+                xs={8}
+                item
+                container
+                direction={"column"}
+                alignContent={"center"}
+                wrap={"nowrap"}
+                alignItems={"center"}
+            >
+                {props.children}
+
+            </Grid>
+
+        </Grid>
+
+
+    );
+};
+
+export default AuthPageWrapper;
--- client/src/Signup.js
+++ client/src/Signup.js
@@ -1,20 +1,42 @@
-import React, { useState } from "react";
-import { Redirect, useHistory } from "react-router-dom";
-import { connect } from "react-redux";
+import React, {useState} from "react";
+import {Redirect, useHistory} from "react-router-dom";
+import {connect} from "react-redux";
 import {
   Grid,
-  Box,
   Typography,
   Button,
   FormControl,
   TextField,
   FormHelperText,
 } from "@material-ui/core";
-import { register } from "./store/utils/thunkCreators";
+import {register} from "./store/utils/thunkCreators";
+import {makeStyles} from "@material-ui/core/styles";
+
+import AuthPageWrapper from "./AuthPageWrapper";
+
+const useStyles = makeStyles(() => ({
+  form: {
+    alignItems: "flex-end",
+    alignSelf: "flex-end",
+    alignContent: "flex-end"
+
+  },
+  label: {
+    color: "#a9a9a9"
+  },
+  shadowButton: {
+    boxShadow: "0 8px 16px 0 rgba(0,0,0,0.1), 0 6px 20px 0 rgba(0,0,0,0.19)"
+  },
+  textField: {
+    width: "50vw"
+  },
+}));
+
 
 const Login = (props) => {
+  const classes = useStyles()
   const history = useHistory();
-  const { user, register } = props;
+  const {user, register} = props;
   const [formErrorMessage, setFormErrorMessage] = useState({});
 
   const handleRegister = async (event) => {
@@ -29,83 +51,140 @@ const Login = (props) => {
       return;
     }
 
-    await register({ username, email, password });
+    await register({username, email, password});
   };
 
   if (user.id) {
-    return <Redirect to="/home" />;
+    return <Redirect to="/home"/>;
   }
 
   return (
-    <Grid container justify="center">
-      <Box>
-        <Grid container item>
-          <Typography>Need to log in?</Typography>
-          <Button onClick={() => history.push("/login")}>Login</Button>
+      <AuthPageWrapper>
+        <Grid container item direction={"row"} xs={9} alignItems={"center"} justify={"flex-end"}>
+          <Grid item xs={5}>
+            <Typography className={classes.label} variant={"h7"}>Already have an account?</Typography>
+          </Grid>
+          <Grid item xs={5}>
+            <Button
+                className={classes.shadowButton}
+                size={"large"}
+                onClick={() => history.push("/login")}
+                variant="raised"
+
+            >
+              <Typography variant={"h7"} color={"primary"}>Login</Typography>
+            </Button>
+          </Grid>
         </Grid>
-        <form onSubmit={handleRegister}>
+
+
+        <Grid xs={9} item container direction={"row"} alignContent={"center"}>
           <Grid>
-            <Grid>
-              <FormControl>
-                <TextField
+            <Typography variant={"h4"}>
+              Create an account.
+            </Typography>
+
+          </Grid>
+          <Grid item className={classes.form}>
+            <SignupForm
+                handleRegister={handleRegister}
+                formErrorMessage={formErrorMessage}
+            />
+          </Grid>
+        </Grid>
+
+      </AuthPageWrapper>
+
+  );
+};
+
+const SignupForm = (props) => {
+  const classes = useStyles();
+  const {handleRegister, formErrorMessage} = props;
+  return (
+      <form onSubmit={handleRegister}>
+        <Grid container direction={"column"}>
+          <Grid item>
+            <FormControl fullWidth>
+              <TextField
+                  className={classes.textField}
                   aria-label="username"
                   label="Username"
                   name="username"
+                  fullWidth
                   type="text"
                   required
-                />
-              </FormControl>
-            </Grid>
-            <Grid>
-              <FormControl>
-                <TextField
+              />
+            </FormControl>
+          </Grid>
+          <Grid item>
+            <FormControl fullWidth>
+              <TextField
+                  className={classes.textField}
                   label="E-mail address"
                   aria-label="e-mail address"
                   type="email"
+                  fullWidth
                   name="email"
                   required
-                />
-              </FormControl>
-            </Grid>
-            <Grid>
-              <FormControl error={!!formErrorMessage.confirmPassword}>
-                <TextField
+              />
+            </FormControl>
+          </Grid>
+          <Grid item>
+            <FormControl error={!!formErrorMessage.confirmPassword}>
+              <TextField
                   aria-label="password"
+                  className={classes.textField}
                   label="Password"
                   type="password"
-                  inputProps={{ minLength: 6 }}
+                  fullWidth
+                  inputProps={{minLength: 6}}
                   name="password"
                   required
-                />
-                <FormHelperText>
-                  {formErrorMessage.confirmPassword}
-                </FormHelperText>
-              </FormControl>
-            </Grid>
-            <Grid>
-              <FormControl error={!!formErrorMessage.confirmPassword}>
-                <TextField
+              />
+              <FormHelperText>
+                {formErrorMessage.confirmPassword}
+              </FormHelperText>
+            </FormControl>
+          </Grid>
+          <Grid item>
+            <FormControl error={!!formErrorMessage.confirmPassword}>
+              <TextField
                   label="Confirm Password"
+                  className={classes.textField}
                   aria-label="confirm password"
                   type="password"
-                  inputProps={{ minLength: 6 }}
+                  fullWidth
+                  inputProps={{minLength: 6}}
                   name="confirmPassword"
                   required
-                />
-                <FormHelperText>
-                  {formErrorMessage.confirmPassword}
-                </FormHelperText>
-              </FormControl>
-            </Grid>
-            <Button type="submit" variant="contained" size="large">
-              Create
-            </Button>
+              />
+              <FormHelperText>
+                {formErrorMessage.confirmPassword}
+              </FormHelperText>
+            </FormControl>
           </Grid>
-        </form>
-      </Box>
-    </Grid>
-  );
-};
+          <Grid item alignItems={"center"} container direction={"row"}>
+            <Grid item xs={4}/>
+            <Grid item xs={4} alignItems={"center"}>
+              <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  size="large"
+                  color={"primary"}
+              >
+                Create
+              </Button>
+            </Grid>
+            <Grid item xs={4}/>
+
+          </Grid>
+
+        </Grid>
+      </form>
+  )
+}
 
 const mapStateToProps = (state) => {
   return {
