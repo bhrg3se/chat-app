60c2c98fb9e5db0f7b72aba3fc0a74b4ca7e5e34
bhrg3se <bhargab@seknox.com>
2021-06-27 13:06:51 +0545

    refactor: configured typescript

--- client/src/routes.tsx
+++ client/src/routes.tsx
@@ -1,13 +1,19 @@
-import React, { useEffect, useState } from "react";
-import { Route, Switch, withRouter } from "react-router-dom";
-import { connect } from "react-redux";
-import { fetchUser } from "./store/utils/thunkCreators";
+import React, {useEffect, useState} from "react";
+import {Route, Switch, withRouter} from "react-router-dom";
+import {connect} from "react-redux";
+import {fetchUser} from "./store/utils/thunkCreators";
 import Signup from "./Signup.js";
 import Login from "./Login.js";
-import { Home, SnackbarError } from "./components";
+import {Home, SnackbarError} from "./components";
+import {User} from "../types";
 
-const Routes = (props) => {
-  const { user, fetchUser } = props;
+
+type RouterProps = {
+  user: User,
+  fetchUser: () => void
+}
+const Routes = (props: RouterProps) => {
+  const {user, fetchUser} = props;
   const [errorMessage, setErrorMessage] = useState("");
   const [snackBarOpen, setSnackBarOpen] = useState(false);
 
