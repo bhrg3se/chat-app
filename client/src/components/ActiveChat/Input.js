dda0d31615c56490d93e11ef3be05b9cf1e63ac7
bhrg3se <bhargab@seknox.com>
2021-06-29 20:47:14 +0545

    refactor: class components converted to functional components: Chat, Home and Input

--- client/src/components/ActiveChat/Input.js
+++ client/src/components/ActiveChat/Input.js
@@ -1,10 +1,10 @@
-import React, { Component } from "react";
-import { FormControl, FilledInput } from "@material-ui/core";
-import { withStyles } from "@material-ui/core/styles";
-import { connect } from "react-redux";
-import { postMessage } from "../../store/utils/thunkCreators";
+import React, {useState} from "react";
+import {FormControl, FilledInput} from "@material-ui/core";
+import {makeStyles} from "@material-ui/core/styles";
+import {connect} from "react-redux";
+import {postMessage} from "../../store/utils/thunkCreators";
 
-const styles = {
+const useStyles = makeStyles(() => ({
   root: {
     justifySelf: "flex-end",
     marginTop: 15,
@@ -15,54 +15,43 @@ const styles = {
     borderRadius: 8,
     marginBottom: 20,
   },
-};
+}));
 
-class Input extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      text: "",
-    };
-  }
+const Input = (props) => {
+  const classes = useStyles();
+  const [text, setText] = useState("")
 
-  handleChange = (event) => {
-    this.setState({
-      text: event.target.value,
-    });
+  const handleChange = (event) => {
+    setText(event.target.value)
   };
 
-  handleSubmit = async (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
     // add sender user info if posting to a brand new convo, so that the other user will have access to username, profile pic, etc.
     const reqBody = {
       text: event.target.text.value,
-      recipientId: this.props.otherUser.id,
-      conversationId: this.props.conversationId,
-      sender: this.props.conversationId ? null : this.props.user,
+      recipientId: props.otherUser.id,
+      conversationId: props.conversationId,
+      sender: props.conversationId ? null : props.user,
     };
-    await this.props.postMessage(reqBody);
-    this.setState({
-      text: "",
-    });
+    await props.postMessage(reqBody);
+    setText("")
   };
 
-  render() {
-    const { classes } = this.props;
     return (
-      <form className={classes.root} onSubmit={this.handleSubmit}>
-        <FormControl fullWidth hiddenLabel>
-          <FilledInput
-            classes={{ root: classes.input }}
-            disableUnderline
-            placeholder="Type something..."
-            value={this.state.text}
-            name="text"
-            onChange={this.handleChange}
-          />
-        </FormControl>
-      </form>
+        <form className={classes.root} onSubmit={handleSubmit}>
+          <FormControl fullWidth hiddenLabel>
+            <FilledInput
+                className={classes.input}
+                disableUnderline
+                placeholder="Type something..."
+                value={text}
+                name="text"
+                onChange={handleChange}
+            />
+          </FormControl>
+        </form>
     );
-  }
 }
 
 const mapStateToProps = (state) => {
@@ -81,6 +70,6 @@ const mapDispatchToProps = (dispatch) => {
 };
 
 export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(withStyles(styles)(Input));
+    mapStateToProps,
+    mapDispatchToProps
+)(Input);
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,64 +1,54 @@
-import React, { Component } from "react";
-import { withStyles } from "@material-ui/core/styles";
-import { Redirect } from "react-router-dom";
-import { connect } from "react-redux";
-import { Grid, CssBaseline, Button } from "@material-ui/core";
-import { SidebarContainer } from "./Sidebar";
-import { ActiveChat } from "./ActiveChat";
-import { logout, fetchConversations } from "../store/utils/thunkCreators";
-import { clearOnLogout } from "../store/index";
+import React, {useEffect, useState} from "react";
+import {makeStyles} from "@material-ui/core/styles";
+import {Redirect} from "react-router-dom";
+import {connect} from "react-redux";
+import {Grid, CssBaseline, Button} from "@material-ui/core";
+import {SidebarContainer} from "./Sidebar";
+import {ActiveChat} from "./ActiveChat";
+import {logout, fetchConversations} from "../store/utils/thunkCreators";
+import {clearOnLogout} from "../store/index";
 
-const styles = {
+const useStyles = makeStyles(() => ({
   root: {
     height: "97vh",
   },
-};
+}));
 
-class Home extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoggedIn: false,
-    };
-  }
+const Home = (props) => {
+  const classes = useStyles();
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
 
-  componentDidUpdate(prevProps) {
-    if (this.props.user.id !== prevProps.user.id) {
-      this.setState({
-        isLoggedIn: true,
-      });
-    }
-  }
+  useEffect(() => {
+    props.fetchConversations();
+  }, [])
 
-  componentDidMount() {
-    this.props.fetchConversations();
-  }
+  useEffect(() => {
+    setIsLoggedIn(true)
+  }, [props.user.id])
 
-  handleLogout = async () => {
-    await this.props.logout(this.props.user.id);
+  const handleLogout = async () => {
+    await props.logout(props.user.id);
   };
 
-  render() {
-    const { classes } = this.props;
-    if (!this.props.user.id) {
-      // If we were previously logged in, redirect to login instead of register
-      if (this.state.isLoggedIn) return <Redirect to="/login" />;
-      return <Redirect to="/register" />;
-    }
-    return (
+  if (!props.user.id) {
+    // If we were previously logged in, redirect to login instead of register
+    if (isLoggedIn) return <Redirect to="/login"/>;
+    return <Redirect to="/register"/>;
+  }
+  return (
       <>
         {/* logout button will eventually be in a dropdown next to username */}
-        <Button className={classes.logout} onClick={this.handleLogout}>
+        <Button onClick={handleLogout}>
           Logout
         </Button>
         <Grid container component="main" className={classes.root}>
-          <CssBaseline />
-          <SidebarContainer />
-          <ActiveChat />
+          <CssBaseline/>
+          <SidebarContainer/>
+          <ActiveChat/>
         </Grid>
       </>
     );
-  }
+
 }
 
 const mapStateToProps = (state) => {
@@ -81,6 +71,6 @@ const mapDispatchToProps = (dispatch) => {
 };
 
 export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(withStyles(styles)(Home));
+    mapStateToProps,
+    mapDispatchToProps
+)(Home);
